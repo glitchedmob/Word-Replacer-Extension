ade0cd6f4a079540dd2e79c4563969cc2214c70d
Levi Zitting <levizitting5663@gmail.com>
2018-11-01 19:45:51 -0500

    Finish UI for options page

--- src/components/ReplacementsManager.vue
+++ src/components/ReplacementsManager.vue
@@ -1,42 +1,69 @@
 <template>
-<div>
-    <v-container>
-        <v-layout>
-            <v-flex>
-                <v-text-field
-                    label="Target word"
-                    v-model="newKey"
-                ></v-text-field>
-            </v-flex>
-            <v-flex>
-                <v-text-field
-                    label="Replacement word"
-                    v-model="newValue"
-                ></v-text-field>
-            </v-flex>
-            <v-flex>
-                <v-btn @click="addWord">Add</v-btn>
-            </v-flex>
-        </v-layout>
+    <div>
+        <v-container>
+            <v-card>
+                <div class="pa-4">
+                    <div class="px-4">
+                        <v-layout>
+                        <v-flex>
+                            <v-text-field
+                                label="Target Word"
+                                v-model="newKey"
+                            ></v-text-field>
+                        </v-flex>
+                        <v-spacer />
+                        <v-flex>
+                            <v-text-field
+                                label="Replacement Word"
+                                v-model="newValue"
+                            ></v-text-field>
+                        </v-flex>
+                        <v-spacer />
+                        <v-flex xs1 class="text-xs-right">
+                            <v-btn @click="addWord">Add</v-btn>
+                        </v-flex>
+                    </v-layout>
+                    </div>
 
-        <v-list>
-            <v-list-tile v-for="(value, key) in words" :key="key">
-                <v-list-tile-content>
-                    <v-list-tile-title>Key: {{ key }}</v-list-tile-title>
-                </v-list-tile-content>
-                <v-list-tile-content>
-                    <v-list-tile-title>Value: {{ value }}</v-list-tile-title>
-                </v-list-tile-content>
-
-                <v-list-tile-action>
-                    <v-btn icon ripple>
-                        <v-icon color="grey lighten-1">trash</v-icon>
-                    </v-btn>
-                </v-list-tile-action>
-            </v-list-tile>
-        </v-list>
-    </v-container>
-</div>
+                    <div v-if="totalWords">
+                        <div class="px-4 py-2">
+                            <v-layout>
+                                <v-flex xs6>
+                                    <p class="body-1 ma-0">
+                                        <strong>Key</strong>
+                                    </p>
+                                </v-flex>
+                                <v-flex xs6>
+                                    <p class="body-1 ma-0">
+                                        <strong>Value</strong>
+                                    </p>
+                                </v-flex>
+                                <v-flex xs1>
+                                </v-flex>
+                            </v-layout>
+                        </div>
+                        <template  v-for="(value, key) in words">
+                            <div :key="`${key}-tile`" class="word-list-item px-4 py-2">
+                                <v-layout align-center>
+                                    <v-flex xs6>
+                                        <p class="body-1 ma-0">{{ key }}</p>
+                                    </v-flex>
+                                    <v-flex xs6>
+                                        <p class="body-1 ma-0">{{ value }}</p>
+                                    </v-flex>
+                                    <v-flex xs1 class="text-xs-right">
+                                        <v-btn icon ripple @click="removeWord(key)" class="delete-button">
+                                            <v-icon color="grey lighten-1">clear</v-icon>
+                                        </v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </div>
+                        </template>
+                    </div>
+                </div>
+            </v-card>
+        </v-container>
+    </div>
 </template>
 
 <script>
@@ -44,9 +71,15 @@
         data: () => ({
             newKey: '',
             newValue: '',
-            words: {}
+            words: {
+                test: 'test'
+            },
         }),
-
+        computed: {
+            totalWords() {
+                return Object.keys(this.words).length;
+            },
+        },
         methods: {
             addWord() {
                 this.words = {
@@ -67,5 +100,9 @@
 </script>
 
 <style lang="stylus">
-
+    .word-list-item
+        transition background 250ms ease-in-out
+        &:hover,
+        &:focus
+            background darken(#FFFFFF, 5%)
 </style>
