bf4b86ab25de7f917e9a47e730a33e492cc3608e
Levi Zitting <levizitting5663@gmail.com>
2018-11-05 16:32:36 -0600

    Add back return true in background

--- src/background.js
+++ src/background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if(request.type === 'getWords') {
         chrome.storage.sync.get(['words'], result => sendResponse(result.words));
+        // Returning true tells chrome that sendResponse will run async
+        return true;
     }
 
     if(request.type === 'saveWords') {
